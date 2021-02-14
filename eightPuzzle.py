a6cd8b1ecf901c477cde389ce53d1052c9abdadd
isaac-18 <isaac.curiel6721@gmail.com>
2021-02-14 15:24:31 -0800

    Fixed duplicate checking and finished MTH

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -7,6 +7,7 @@ ROWS, COLS = (3, 3)
 
 nodesExpanded = 0
 maxQueueSize = 1
+visitedStates = []
 
 class Node:
     def __init__(self, data, g):
@@ -19,10 +20,11 @@ class Node:
         return
 
     def add_child(self, child):
-        # global nodesExpanded
         if child not in self.children:
+        # if child not in nodes:
             self.children.append(child)
-            # nodesExpanded += 1
+        else:
+            print('Duplicate')
 
     def state(self):
         return self.data
@@ -77,15 +79,22 @@ def welcome():
     algoChoice = int(input())
 
 
-def UCS(start):
+def UCS(node):
     h = 0
-    return start.g + h
+    return node.g + h
 
-def MTH(start, goal):
+def MTH(node, goal):
+    puzzle = node.data
+    h = 0
+
     # Calculate number of misplaced tiles for h
-    return
+    for y in range(COLS):
+        for x in range(ROWS):
+            if puzzle[y][x] != goal[y][x]:
+                h +=1
+    return node.g + h
 
-def MDH(start, goal):
+def MDH(node, goal):
     # Distance of blank square to goal position for h
     return
 
@@ -99,13 +108,14 @@ def expand(node, queue):
             # Move left
             # print('Moving left')
             newPuzzle = deepcopy(puzzle)
-            print(coords)
             if coords[1] != 0:
                 newPuzzle[coords[0]][coords[1]], newPuzzle[coords[0]][coords[1]-1] = newPuzzle[coords[0]][coords[1]-1], newPuzzle[coords[0]][coords[1]]
                 newNode = Node(newPuzzle, g)
-                if (newPuzzle != puzzle) and (newNode not in queue):
+                # if (newPuzzle != puzzle) and (newPuzzle not in visitedStates):
+                if newPuzzle not in visitedStates:
                     node.add_child(newNode)
-                    nodesExpanded += 1
+                else:
+                    print('node is already in queue')
         except:
             # print('Can\'t move left!')
             # print('-------------')
@@ -117,9 +127,11 @@ def expand(node, queue):
             newPuzzle = deepcopy(puzzle)
             newPuzzle[coords[0]][coords[1]], newPuzzle[coords[0]][coords[1]+1] = newPuzzle[coords[0]][coords[1]+1], newPuzzle[coords[0]][coords[1]]
             newNode = Node(newPuzzle, g)
-            if (newPuzzle != puzzle) and (newNode not in queue):
+            # if (newPuzzle != puzzle) and (newPuzzle not in visitedStates):
+            if newPuzzle not in visitedStates:
                 node.add_child(newNode)
-                nodesExpanded += 1
+            else:
+                print('node is already in queue')
         except:
             # print('Can\'t move right!')
             # print('-------------')
@@ -132,9 +144,11 @@ def expand(node, queue):
             if coords[0] != 0:
                 newPuzzle[coords[0]][coords[1]], newPuzzle[coords[0]-1][coords[1]] = newPuzzle[coords[0]-1][coords[1]], newPuzzle[coords[0]][coords[1]]
                 newNode = Node(newPuzzle, g)
-                if (newPuzzle != puzzle) and (newNode not in queue):
+                # if (newPuzzle != puzzle) and (newPuzzle not in visitedStates):
+                if newPuzzle not in visitedStates:
                     node.add_child(newNode)
-                    nodesExpanded += 1
+                else:
+                    print('node is already in queue')
         except:
             # print('Can\'t move up!')
             # print('-------------')
@@ -146,9 +160,11 @@ def expand(node, queue):
             newPuzzle = deepcopy(puzzle)
             newPuzzle[coords[0]][coords[1]], newPuzzle[coords[0]+1][coords[1]] = newPuzzle[coords[0]+1][coords[1]], newPuzzle[coords[0]][coords[1]]
             newNode = Node(newPuzzle, g)
-            if (newPuzzle != puzzle) and (newNode not in queue):
+            # if (newPuzzle != puzzle) and (newPuzzle not in visitedStates):
+            if newPuzzle not in visitedStates:
                 node.add_child(newNode)
-                nodesExpanded += 1
+            else:
+                print('node is already in queue')
         except:
             # print('Can\'t move down!')
             # print('-------------')
@@ -159,6 +175,9 @@ def generalSearch(puzzleStart, puzzleEnd, queueFunc):
     rootNode = Node(puzzleStart, 0)
     nodes = []
     heapq.heappush(nodes, (0, 0, rootNode))
+
+    # global visitedStates
+    # visitedStates = []
 
     global nodesExpanded
     maxQueueSize = 1
@@ -173,8 +192,13 @@ def generalSearch(puzzleStart, puzzleEnd, queueFunc):
             return
         
         node = heapq.heappop(nodes)[2]
+        
+        visitedStates.append(node.data)
+
         print('Popped')
         print(node.state())
+        nodesExpanded += 1
+        
 
         if node.state() == puzzleEnd:
             print('Success!')
@@ -184,6 +208,7 @@ def generalSearch(puzzleStart, puzzleEnd, queueFunc):
             # return node ?
             return
         
+        # Uniform cost search
         if queueFunc == 1:
             # Expands the parent node and places all the children in queue.
             # Priority queue deals with ordering. Then we pop off highest priority 
@@ -194,41 +219,84 @@ def generalSearch(puzzleStart, puzzleEnd, queueFunc):
             print('Expanding {} children'.format(len(node.get_children())))
             for child in node.get_children():
                 queuePosition += 1
-                if child not in nodes:
+                if child.data not in visitedStates:
                     heapq.heappush(nodes, (UCS(child), queuePosition, child))
-                    child.display()
-                    print('-------------')
-                    # nodesExpanded += 1
-            print('Next level')
-            print('===============')
+            #         child.display()
+            #         print('-------------')
+            # #         # nodesExpanded += 1
+            # print('Next level')
+            # print('===============')
 
-        # elif queueFunc == 2:
-        #     node.expand()
-        #     nodesExpanded += 1
-        #     for child in node.children:
-        #         nodes.put(MTH(child, puzzleEnd), child)
-        # elif queueFunc == 3:
-        #     node.expand()
-        #     nodesExpanded += 1
-        #     for child in node.children:
-        #         nodes.put(MDH(child, puzzleEnd), child)
+        # Misplaced tile heuristic
+        elif queueFunc == 2:
+            expand(node, nodes)
+            print('Expanding {} children'.format(len(node.get_children())))
+            for child in node.get_children():
+                queuePosition += 1
+                if child.data not in visitedStates:
+                    heapq.heappush(nodes, (MTH(child, puzzleEnd), queuePosition, child))
+        elif queueFunc == 3:
+            expand(node, nodes)
+            print('Expanding {} children'.format(len(node.get_children())))
+            for child in node.get_children():
+                queuePosition += 1
+                if child.data not in visitedStates:
+                    heapq.heappush(nodes, (MDH(child, puzzleEnd), queuePosition, child))
     
 
 def main():
     # welcome()
     puzzle = [[0 for i in range(COLS)] for j in range(ROWS)]
+    
     puzzle = [[1, 2, 3], 
               [4, 5, 6], 
               [0, 7, 8]]
-    # puzzle = [[1, 2, 3], 
-    #           [5, 0, 6], 
-    #           [4, 7, 8]] # AB 23 iterations 4 depth
+    puzzle1 = [[1, 2, 3], 
+              [5, 0, 6], 
+              [4, 7, 8]] # AB 23 iterations 4 depth
     
+    A = [[1, 2, 3], # 0
+        [4, 5, 6],
+        [7, 8, 0]]
+
+    B = [[1, 2, 3], 
+        [4, 5, 6],  # 2
+        [0, 7, 8]]
+
+    C = [[1, 2, 3], 
+        [5, 0, 6],  # 4
+        [4, 7, 8]]
+
+    D = [[1, 3, 6], 
+        [5, 0, 2],  # 8
+        [4, 7, 8]]
+
+    E = [[1, 3, 6], 
+        [5, 0, 7],  # 12
+        [4, 8, 2]]
+
+    F = [[1, 6, 7], # 16
+        [5, 0, 3],
+        [4, 8, 2]]
+
+    G = [[7, 1, 2], 
+        [4, 8, 5],  # 20
+        [6, 3, 0]]
+
+    H = [[0, 7, 2], # 24
+        [4, 6, 1],
+        [3, 5, 8]]
+
+    I = [[8, 6, 7], # 31
+        [2, 5, 4],
+        [3, 0, 1]]
+
+
     puzzleEnd = [[1, 2, 3], 
                  [4, 5, 6], 
                  [7, 8, 0]]
 
-    generalSearch(puzzle, puzzleEnd, 1)
+    generalSearch(F, puzzleEnd, 1)
     
     
 main()
